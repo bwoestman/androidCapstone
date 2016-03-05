50fd40049154372b1d719be7883e8c6061ca36ce
Brian Woestman <woestmab@gmail.com>
2016-03-05 03:40:40 -0600

    get request is working from dark sky

--- WeatherAlarm/app/src/main/java/com/example/bwoestman/weatheralarm/AppInfo.java
+++ WeatherAlarm/app/src/main/java/com/example/bwoestman/weatheralarm/AppInfo.java
@@ -8,4 +8,5 @@ package com.example.bwoestman.weatheralarm;
 public interface AppInfo
 {
     public static final String TAG = "*****";
+    public static final String API_KEY = "384e291c8f22981980d220fd0400870a";
 }
--- WeatherAlarm/app/src/main/java/com/example/bwoestman/weatheralarm/MainActivity.java
+++ WeatherAlarm/app/src/main/java/com/example/bwoestman/weatheralarm/MainActivity.java
@@ -4,6 +4,13 @@ import android.annotation.TargetApi;
 import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import com.johnhiott.darkskyandroidlib.*;
+import com.johnhiott.darkskyandroidlib.models.Request;
+import com.johnhiott.darkskyandroidlib.models.WeatherResponse;
+import retrofit.Callback;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
 
 import java.util.ArrayList;
 
@@ -14,8 +21,32 @@ public class MainActivity extends AppCompatActivity implements AppInfo
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
+        ForecastApi.create(API_KEY);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RequestBuilder weather = new RequestBuilder();
+
+        final Request request = new Request();
+        request.setLat("43.06");
+        request.setLng("89.40");
+        request.setUnits(Request.Units.US);
+        request.setLanguage(Request.Language.ENGLISH);
+
+        weather.getWeather(request, new Callback<WeatherResponse>()
+        {
+            @Override
+            public void success(WeatherResponse weatherResponse, Response response)
+            {
+                Log.d(TAG, "success: " + weatherResponse.getCurrently().getPrecipProbability());
+            }
+
+            @Override
+            public void failure(RetrofitError error)
+            {
+                Log.d(TAG, "failure: Oh, shit!" + error);
+            }
+        });
 
         if (findViewById(R.id.fragment_container) != null)
         {
